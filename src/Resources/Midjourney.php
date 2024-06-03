a53fdaea19d4ac93dfb50e8320512bf96be76b90
Shu Chen <shuch3n90@gmail.com>
2024-06-03 17:08:46 +0200

    Missing payload

--- src/Resources/Midjourney.php
+++ src/Resources/Midjourney.php
@@ -5,7 +5,7 @@ namespace eDiasoft\Midjourney\Resources;
 class Midjourney
 {
     public const APPLICATION_ID = 936929561302675456;
-    public const IMAGINE_DATA_VERSION = "1166847114203123795";
+    public const IMAGINE_DATA_VERSION = "1237876415471554623";
     public const IMAGINE_DATA_ID = "938956540159881230";
     public const INFO_DATA_VERSION = "1118961510123847776";
     public const INFO_DATA_ID = "972289487818334209";
